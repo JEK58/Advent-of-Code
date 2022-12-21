8c0e36464bbcedee168cfb0a6d710c8445e4568a
Stephan Schöpe <socialcrap+github@posteo.de>
2022-12-21 20:17:23 +0100

    day 21

--- 21/a.ts
+++ 21/a.ts
@@ -5,7 +5,7 @@ function getMonkey(name: string) {
 
   input
     .split(/\r?\n/g)
-    .filter((c) => c.length < 10)
+    .filter((c) => c.length < 11)
     .map((c) => c.split(": "))
     .map((m) => yellingMonkeys.set(m[0], +m[1]));
 
@@ -32,5 +32,68 @@ function getMonkey(name: string) {
   }
   return yellingMonkeys.get(name);
 }
+
+function getHuman() {
+  const yellingMonkeys = new Map();
+  const root = input
+    .split(/\r?\n/g)
+    .filter((c) => c.includes("root"))
+    .map((c) => c.split(" "))
+    .flat();
+
+  let left = root[1];
+  let right = root[3];
+
+  input
+    .split(/\r?\n/g)
+    .filter((c) => !c.includes("humn"))
+    .filter((c) => c.length < 11)
+    .map((c) => c.split(": "))
+    .map((m) => yellingMonkeys.set(m[0], +m[1]));
+
+  const waitingMonkeys = input
+    .split(/\r?\n/g)
+    .filter((c) => !c.includes("root"))
+    .filter((c) => c.length > 11)
+    .map((c) => c.split(": "));
+
+  let diff = "";
+  while (left != diff) {
+    diff = left;
+    for (const monkey of waitingMonkeys) {
+      left = left.replaceAll(monkey[0], `(${monkey[1]})`);
+    }
+  }
+
+  diff = "";
+  while (right != diff) {
+    diff = right;
+    for (const monkey of waitingMonkeys) {
+      right = right.replaceAll(monkey[0], `(${monkey[1]})`);
+    }
+  }
+
+  for (const monkey of yellingMonkeys) {
+    left = left.replaceAll(monkey[0], monkey[1]);
+    right = right.replaceAll(monkey[0], monkey[1]);
+  }
+
+  const goal = eval(left.includes("humn") ? right : left);
+
+  let lo = 0;
+  let hi = 1e20;
+  let humn; // mid
+  while (hi > lo) {
+    humn = Math.floor((hi + lo) / 2);
+    const res = goal - eval(left.includes("humn") ? left : right);
+    if (res == 0) return humn;
+    // TODO: The "< or >" needs to be switched manuallay depending on the input
+    else if (res < 0) lo = humn;
+    else hi = humn;
+  }
+}
 // 223971851179174
 console.log(getMonkey("root"));
+
+// 3379022190351
+console.log(getHuman());
